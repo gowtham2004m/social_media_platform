276ade08e4730369f19a6bdd2dfdf575dcfcd077
gowtham <gowthamm.21cse@kongu.edu>
2024-08-07 15:22:56 +0530

    url updated

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://66b33f964cf402a87a4d763b--postingplatform.netlify.app/"
+    // baseURL: "https://66b33f964cf402a87a4d763b--postingplatform.netlify.app/"
+    baseURL: "https://master--postingplatform.netlify.app/"
 });
     
